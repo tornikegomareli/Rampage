df65e8c3347ef314a2faed74386bceaae8f69dab
Tornike Gomareli <gomarelidevelopment@gmail.com>
2022-02-24 03:40:48 +0400

    Initial Commit

---
+++ Rampage/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Rampage
+//
+//  Created by Tornike Gomareli on 24.02.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
